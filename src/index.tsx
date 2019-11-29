4f0df4181be6b5398124705f9464b148832e63d3
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-11-28 22:16:00 -0300

    Reorganized CSS

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,7 @@ import '@ionic/react/css/display.css'
 
 /* Theme variables */
 import './theme/variables.css'
+import './theme/theme.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
